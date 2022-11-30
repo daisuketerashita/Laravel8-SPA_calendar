0c4bed4675cf0f4e1299875b09420673d80576fb
寺下大典 <ekusiad.tera@icloud.com>
2022-11-30 20:52:03 +0900

    レイアウト修正

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}">
+    <title>追加ページ</title>
 </head>
+
 <body>
-    <div>
-        <form action="{{ route('store',['id' => $day]) }}" method="post" enctype="multipart/form-data">
-            @csrf
-            <table>
-                <tr>
-                    <th>日付</th>
-                    <td><input type='text' class='form-control' name='sch_date' value="{{ $day }}"></td>
-                </tr>
-                <tr>
-                    <th>部位</th>
-                    <td>
-                        <input type="radio" name="sch_part" value="胸">胸
-                        <input type="radio" name="sch_part" value="脚">脚
-                        <input type="radio" name="sch_part" value="背中">背中
-                        <input type="radio" name="sch_part" value="腕">腕
-                        <input type="radio" name="sch_part" value="肩">肩
-                    </td>
-                </tr>
-            </table>
-            <input type='submit' class='' value='種目管理画面へ'>
-        </form>
-    </div>
-    <a href="{{ route('index') }}">戻る</a>
+    <div class="form-wrapper">
+        <div class="form-content">
+            <form action="{{ route('store',['id' => $day]) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="inner-wrapper">
+                    <h3>日付</h3>
+                    <input type='text' class='form-control' name='sch_date' value="{{ $day }}">
+                    <h3>部位</h3>
+                    <input type="radio" name="sch_part" value="胸">胸
+                    <input type="radio" name="sch_part" value="脚">脚
+                    <input type="radio" name="sch_part" value="背中">背中
+                    <input type="radio" name="sch_part" value="腕">腕
+                    <input type="radio" name="sch_part" value="肩">肩
+                    <div class="inner-button">
+                    <p><input type='submit' class='form-btn next-btn' value='種目管理画面へ'></p>
+                    <p><button type="button" class='form-btn prev-btn' onclick="location.href='{{ route('index') }}' ">戻る</button></p>
+                    </div>
+                </div>
+            </form>
+        </div><!-- /.form-content -->
+    </div><!-- /.form-wrapper -->
 </body>
+
 </html>
