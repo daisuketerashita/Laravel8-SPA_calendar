1b48737a97dd33bc205be104517c0c07ecc94964
寺下大典 <ekusiad.tera@icloud.com>
2022-12-11 15:35:22 +0900

    編集ページデザイン調整

--- resources/views/exercise/edit.blade.php
+++ resources/views/exercise/edit.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/css/exercise.css')  }}">
     <link rel="stylesheet" href="{{ asset('/css/edit.css')  }}">
     <title>種目編集ページ</title>
 </head>
@@ -13,28 +12,30 @@
 <body>
     <div class="form-wrapper">
         <div class="form-content">
+            <h3>{{ $schedule->start_date }}</h3>
+            <h3>{{ $schedule->sch_part }}</h3>
             <form action="" method="post" enctype="multipart/form-data">
                 @csrf
                 <table class="inner-wrapper">
                     <tr>
                         <th><label for="name">種目名：</label></th>
-                        <td><input type='text' class='sch_date' name='name'></td>
+                        <td><input type='text' class='sch_date' name='name' value="{{ $exercise->name }}"></td>
                     </tr>
                     <tr>
                         <th><label for="weight">重さ：</label></th>
-                        <td><input type="number" name="weight"> kg</td>
+                        <td><input type="number" name="weight" value="{{ $exercise->weight }}"> kg</td>
                     </tr>
                     <tr>
                         <th><label for="repetition">レップ数：</label></th>
-                        <td><input type="number" name="repetition"> 回</td>
+                        <td><input type="number" name="repetition" value="{{ $exercise->repetition }}"> 回</td>
                     </tr>
                     <tr>
                         <th><label for="set_num">セット数：</label></th>
-                        <td><input type="number" name="set_num"> セット</td>
+                        <td><input type="number" name="set_num" value="{{ $exercise->set_num }}"> セット</td>
                     </tr>
                     <tr>
                         <th class="memo_th"><label for="exe_contents">メモ：</label></th>
-                        <td class="memo_td"><textarea name="exe_contents" id="" cols="30" rows="10"></textarea></td>
+                        <td class="memo_td"><textarea name="exe_contents" id="" cols="30" rows="10">{{ $exercise->exe_contents }}</textarea></td>
                     </tr>
                     <tr class="inner-button">
                         <td colspan="2"><input type='submit' class='form-btn next-btn' value='編集'></td>
